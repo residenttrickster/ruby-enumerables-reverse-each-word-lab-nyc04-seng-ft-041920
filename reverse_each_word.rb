ecc9a653d01e3d12e010bff353c892a012b31ce5
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-30 15:13:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
 array = string.split(" ")
 new_array = []
 array.each do |element|
+  new_array << element.reverse
   element.reverse
 end
-new_array << element.reverse
 new_array
 end
